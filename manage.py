732dd8c9751d6cdd75901a30febd6d9dc1403b07
Pablo <pablotejada98@gmail.com>
2022-10-17 18:07:22 +0200

    test

--- manage.py
+++ manage.py
@@ -3,6 +3,61 @@
 import os
 import sys
 
+import json
+
+from opentelemetry import trace as OpenTelemetry
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider, sampling
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+)
+
+merged = dict()
+for name in ["dt_metadata_e617c525669e072eebe3d0f08212e8f2.json", "/var/lib/dynatrace/enrichment/dt_metadata.json"]:
+    try:
+        data = ''
+        with open(name) as f:
+          data = json.load(f if name.startswith("/var") else open(f.read()))
+        merged.update(data)
+    except:
+        pass
+
+merged.update({
+    "service.name": "python-quickstart", #TODO Replace with the name of your application
+    "service.version": "1.0.1", #TODO Replace with the version of your application
+})
+resource = Resource.create(merged)
+
+tracer_provider = TracerProvider(sampler=sampling.ALWAYS_ON, resource=resource)
+OpenTelemetry.set_tracer_provider(tracer_provider)
+
+tracer_provider.add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter(
+        endpoint="http://localhost:14499/otlp/v1/traces"
+    )))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
     """Run administrative tasks."""
